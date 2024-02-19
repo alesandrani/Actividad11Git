b5cb6392c3b6b00b7d85241242243c2db552b2b8
alesandrani <alesandrani@outlook.es>
2024-02-19 12:59:10 +0100

    mostrar jugadores

--- ActividadGit11/src/git/Equipo.java
+++ ActividadGit11/src/git/Equipo.java
@@ -1,6 +1,29 @@
 package git;
 
+import java.util.Arrays;
+
 public class Equipo {
 	String nombre;
 	String [ ]jugadores;
+	public Equipo() {
+		super();
+	}
+	
+	public Equipo(String nombre, String[] jugadores) {
+		super();
+		this.nombre = nombre;
+		this.jugadores = jugadores;
+	}
+
+	@Override
+	public String toString() {
+		return "Equipo [nombre=" + nombre + ", jugadores=" + Arrays.toString(jugadores) + "]";
+	}
+	//??
+	public void mostrarJugadores() {
+        System.out.println("Jugadores del equipo " + nombre + ":");
+        for (String jugador : jugadores) {
+            System.out.println(jugador);
+        }
+    }
 }
