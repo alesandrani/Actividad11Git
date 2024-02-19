57b6c5483d6e077f3e3eb084f9581bbeae557c81
Yinary_Hdz <hernandezyinary@gmail.com>
2024-02-19 13:03:39 +0100

    metodos: existeJugador y cantidadDeJugadores

--- ActividadGit11/src/git/Equipo.java
+++ ActividadGit11/src/git/Equipo.java
@@ -3,4 +3,22 @@ package git;
 public class Equipo {
 	String nombre;
 	String [ ]jugadores;
+	
+	public boolean existeJugador(String jugador) {
+		for (String j : jugadores) {
+			if (j.equalsIgnoreCase(jugador)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	public int cantidadDeJugadores() {
+		int contadorJugadores = 0;
+		for(String j : jugadores) {
+			if(j != null) {
+				contadorJugadores++;
+			}
+		}
+		return contadorJugadores;
+	}
 }
